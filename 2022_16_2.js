791ebb82aa5edd4532dbf36f757c149f4e034e8a
hchulvej <hchulvej@gmail.com>
2022-12-29 16:12:56 +0100

    VS Code

--- 2022_16_2.js
+++ 2022_16_2.js
@@ -6,7 +6,7 @@ const openFile = (filename) => {
     return data.split('\r\n');
 }
 
-const puzzleData = openFile('2022_16_small.txt');
+const puzzleData = openFile('2022_16.txt');
 
 const parseLine = (line) => {
     let name = line.split(' ')[1];
@@ -42,8 +42,41 @@ for (const v of rawValves) {
     indexedValves.push(v);
 }
 
-const flowingValves = indexedValves.filter(v => v[1] > 0);
+const flowingValves = indexedValves.filter(v => v[1] > 0).map(v => v[0]);
 
-let state = '0'.repeat(flowingValves.length);
+let DP = new Map();
 
-console.log(state, flowingValves);
+const score = (position, valves, openValves, timeLeft) => {
+
+    if (timeLeft === 0) {
+        return 0;
+    }
+
+    let key = `p=${position}.${valves.join('v')}.${Array.from(openValves).join('o')}.${timeLeft}`;
+
+    if (DP.has(key)) {
+        return DP.get(key);
+    }
+
+    let ans = 0;
+    let alreadyOpen = openValves.has(position);
+
+
+    if (!alreadyOpen && flowingValves.includes(position)) {
+        let newOpenValves = new Set([position]);
+        openValves.forEach(x => newOpenValves.add(x));
+        ans = Math.max(ans, indexedValves[position][1] * (timeLeft - 1) + score(position, valves, newOpenValves, timeLeft - 1));
+    }
+    
+    for (const newPos of indexedValves[position][2].filter(x => valves.includes(x))) {
+        ans = Math.max(ans, score(newPos, valves, openValves, timeLeft - 1));
+    }
+
+    DP.set(key, ans);
+    currOpen = openValves;
+
+    return ans;
+}
+
+console.log(score(0, [...Array(indexedValves.length).keys()], new Set(), 30));
+

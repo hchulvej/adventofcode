801d938cb48f5bf028a5193e56fe1a8fa790d5fe
hchulvej <hchulvej@gmail.com>
2022-12-29 17:53:10 +0100

    VS Code

---
+++ 2022_16_3.js
@@ -0,0 +1,97 @@
+const fs = require('fs');
+
+const openFile = (filename) => {
+    let data = fs.readFileSync(filename, {encoding: 'utf-8'});
+
+    return data.split('\r\n');
+}
+
+const puzzleData = openFile('2022_16_small.txt');
+
+const parseLine = (line) => {
+    let name = line.split(' ')[1];
+    let flowrate = Number(line.split('=')[1].split(';')[0]);
+    let valves;
+    if (line.includes('valves')) {
+        valves = line.split('valves')[1].trim().split(', ');
+    } else {
+        valves = [line.split(' ').pop()];
+    }
+    return [name, flowrate, valves];
+}
+
+let rawValves = [];
+puzzleData.forEach(line => rawValves.push(parseLine(line)));
+
+
+
+rawValves.sort((v1, v2) => {
+    if (v1[0] === 'AA' || v2[0] === 'AA') {
+        return Number.POSITIVE_INFINITY;
+    }
+    return v2[1] - v1[1];
+});
+
+// console.log(rawValves[8])
+
+let translate = {};
+for (const v of rawValves) {
+    translate[v[0]] = rawValves.indexOf(v);
+}
+
+// Translate to indices
+for (const v of rawValves) {
+    v[0] = translate[v[0]];
+    v[2] = v[2].map(t => translate[t]);
+}
+
+// Flowing valves
+const flowingValves = rawValves.filter(valve => valve[1] > 0);
+const nonFlowingValves = rawValves.filter(valve => valve[1] === 0 && valve[0] > 0);
+
+
+const valveIndices = rawValves.map(valve => valve[0]);
+const flowingIndices = flowingValves.map(valve => valve[0]);
+const nonFlowingIndices = nonFlowingValves.map(valve => valve[0]);
+
+// Creating distances from every node to every other node
+let dist = new Map();
+
+for (const ind of valveIndices) {
+
+    dist.set(ind, new Map());
+    dist.get(ind).set(ind, 0).set(0, 0);
+
+    let visited = new Set();
+    visited.add(ind);
+
+    let queue = [[0, ind]];
+
+    while (queue.length > 0) {
+        let [distance, position] = queue.shift();
+        for (const neighbor of rawValves[position][2]) {
+            if (visited.has(neighbor)) {
+                continue;
+            } else {
+                visited.add(neighbor);
+                dist.get(ind).set(neighbor, distance + 1);
+                queue.push([distance + 1, neighbor]);
+            }
+        }
+    }
+
+    dist.get(ind).delete(ind);
+    if (ind > 0) {
+        dist.get(ind).delete(0);
+    }
+}
+
+// Clean up the non-flowing valves
+for (const ind of valveIndices) {
+    for (const nf of nonFlowingIndices) {
+        if (dist.get(ind).has(nf)) {
+            dist.get(ind).delete(nf);
+        }
+    }
+}
+

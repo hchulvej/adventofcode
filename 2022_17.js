af6584017f0dd5030b6f437b3271424148179b01
hchulvej <hchulvej@gmail.com>
2023-01-02 10:56:13 +0100

    VS Code

---
+++ 2022_17.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const jsc = require('js-combinatorics');
+
+const openFile = (filename) => {
+    let data = fs.readFileSync(filename, {encoding: 'utf-8'});
+
+    return data.split('\r\n');
+}
+
+const puzzleData = openFile('2022_17_small.txt');
+
+
+/*
+    Setting up the cave
+
+    7 units wide (x = 0..6)
+
+    floor is at y-level 0
+*/

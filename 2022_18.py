07b89eb7112d2af1e91ba66a38ce2218ed654b66
hchulvej <hchulvej@gmail.com>
2023-01-07 14:39:14 +0100

    Python solution to both parts

--- 2022_18.py
+++ 2022_18.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 """
     Load and parse data
 """
@@ -19,11 +21,11 @@ min_z: int = min([t[2] for t in data])
 max_z: int = max([t[2] for t in data])
 
 def out_of_bounds(t):
-    return not ((min_x - 1 < t[0] < max_x + 1) and (min_y - 1 < t[1] < max_y + 1) and (min_z - 1 < t[2] < max_z + 1))
+    return not ((min_x - 2 < t[0] < max_x + 2) and (min_y - 2 < t[1] < max_y + 2) and (min_z - 2 < t[2] < max_z + 2))
 
 def neighbours(t):
     deltas = [(1,0,0), (-1,0,0), (0,1,0), (0,-1, 0), (0,0,1), (0,0,-1)]
-    return set([(t[0] + delta[0], t[1] + delta[1], t[2] + delta[2]) for delta in deltas if not out_of_bounds(((t[0] + delta[0], t[1] + delta[1], t[2] + delta[2])))])
+    return set([(t[0] + delta[0], t[1] + delta[1], t[2] + delta[2]) for delta in deltas])
 
 
 """
@@ -33,3 +35,54 @@ def exposed_faces(t):
     return 6 - len([n for n in neighbours(t) if n in data])
 
 print(sum([exposed_faces(t) for t in data]))
+
+
+"""
+    Part Two
+"""
+free_air = set()
+trapped_air = set()
+all_points = set()
+
+def trapped(t):
+    
+    if t in trapped_air:
+        return True
+    
+    if t in free_air:
+        return False
+    
+    if t in data:
+        return False
+    
+    queue = deque([t])
+    
+    visited = set()
+    
+    while queue:
+        
+        node = queue.popleft()
+        
+        if node not in visited:
+            visited.add(node)
+            for n in neighbours(node):
+                if n not in data:
+                    queue.append(n)
+                    if out_of_bounds(n):
+                        free_air.add(t)
+                        return False
+    
+    trapped_air.add(t)
+    return True
+
+for x in range(min_x - 1, max_x + 2):
+    for y in range(min_y - 1, max_y + 2):
+        for z in range(min_z - 1, max_z + 2):
+            all_points.add((x, y, z))
+            trapped((x, y, z))
+
+
+def exposed_faces_p2(t):
+    return 6 - len([n for n in neighbours(t) if n in data.union(trapped_air)])
+
+print(sum([exposed_faces_p2(t) for t in data]))                 

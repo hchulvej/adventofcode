e74184dd17342d05fe85571658b86fd622238263
hchulvej <hchulvej@gmail.com>
2023-01-08 19:13:44 +0100

    WIP

--- 2022_19.py
+++ 2022_19.py
@@ -1,11 +1,12 @@
 import re
 from dataclasses import dataclass
+from functools import cache
 
 """
     Load and parse data
 """
 
-with open('./2022_19_small.txt', "r", encoding="utf-8") as file:
+with open('./2022_19.txt', "r", encoding="utf-8") as file:
     data = list()
     for line in file:
         data.append(tuple([int(x) for x in re.findall(r'\d+', line)]))
@@ -17,7 +18,7 @@ with open('./2022_19_small.txt', "r", encoding="utf-8") as file:
 def blueprint(t) -> dict:
     return {'no': t[0], 'ore_robot_price': [t[1], 0, 0], 'clay_robot_price': [t[2], 0, 0], 'obsidian_robot_price': [t[3], t[4], 0], 'geode_robot_price': [t[5], 0, t[6]]}
 
-@dataclass
+@dataclass(unsafe_hash=True)
 class State:
     time_left: int
     ore_robots: int
@@ -33,14 +34,24 @@ class State:
         return dict(zip(['time_left', 'ore_robots', 'clay_robots', 'obsidian_robots', 'geode_robots', 'ore_deposit', 'clay_deposit', 'obsidian_deposit', 'geodes_opened'], [self.time_left, self.ore_robots, self.clay_robots, self.obsidian_robots, self.geode_robots, self.ore_deposit, self.clay_deposit, self.obsidian_deposit, self.geodes_opened]))
 
     def get_signature(self):
-        return [self.time_left, self.ore_robots, self.clay_robots, self.obsidian_robots, self.geode_robots, self.ore_deposit, self.clay_deposit, self.obsidian_deposit, self.geodes_opened].join(".")
+        return ".".join([str(e) for e in [self.time_left, self.ore_robots, self.clay_robots, self.obsidian_robots, self.geode_robots, self.ore_deposit, self.clay_deposit, self.obsidian_deposit, self.geodes_opened]])
 
-def move_on(state: State) -> State:
+# Hint from hyper-neutrino
+def max_spending(t: tuple) -> tuple:
+    bp = blueprint(t)
+    keys = ['ore_robot_price', 'clay_robot_price', 'obsidian_robot_price', 'geode_robot_price']
+    res = []
+    for i in range(3):
+        res.append(max([bp[key][i] for key in keys]))
+    return res
+
+def move_on(state: State, bp: tuple) -> State:
+    ms = max_spending(bp)
     old_state = state.get_state()
     old_state['time_left'] -= 1
-    old_state['ore_deposit'] += old_state['ore_robots']
-    old_state['clay_deposit'] += old_state['clay_robots']
-    old_state['obsidian_deposit'] += old_state['obsidian_robots']
+    old_state['ore_deposit'] = min(old_state['ore_deposit'] + old_state['ore_robots'], ms[0] * old_state['time_left'])
+    old_state['clay_deposit'] = min(old_state['clay_deposit'] + old_state['clay_robots'], ms[1] * old_state['time_left'])
+    old_state['obsidian_deposit'] = min(old_state['obsidian_deposit'] + old_state['obsidian_robots'], ms[2] * old_state['time_left'])
     old_state['geodes_opened'] += old_state['geode_robots']
     return State(*old_state.values())
     
@@ -83,37 +94,52 @@ def buy_robot(state: State, type: str, bp: tuple) -> State:
         old_state['geodes_opened'] -= 1 #because of move_on
         return State(*old_state.values())
 
-max_opened_geodes = 0
+def combined_move(state: State, type: str, bp: tuple) -> State:
+    if type == 'wait':
+        return move_on(state, bp)
+    if type == 'ore':
+        if afford_robot(state, 'ore', bp):
+            state = buy_robot(state, 'ore', bp)
+        return move_on(state, bp)
+    if type == 'clay':
+        if afford_robot(state, 'clay', bp):
+            state = buy_robot(state, 'clay', bp)
+        return move_on(state, bp)
+    if type == 'obsidian':
+        if afford_robot(state, 'obsidian', bp):
+            state = buy_robot(state, 'obsidian', bp)
+        return move_on(state, bp)
+    if type == 'geode':
+        if afford_robot(state, 'geode', bp):
+            state = buy_robot(state, 'geode', bp)
+        return move_on(state, bp)  
 
-# DFS
 
-cache = dict()
-for bp in data:
-    cache[bp] = dict()
 
-def dfs(bp: tuple, state: State) -> int:
+"""
+    Part One: DFS
+"""
+@cache
+def dfs(state: State, bp: tuple) -> int:
     
-    if state.get_signature() in cache[bp]:
-        return cache[bp][state.get_signature()]
+    if state.get_state()['time_left'] == 0:
+        return state.get_state()['geodes_opened']
     
-    visited = set()
+    key = state.get_signature()
     
-    stack = [state]
+    # Worst case maxval: do nothing
+    maxval = state.get_state()['geodes_opened'] + state.get_state()['time_left'] * state.get_state()['geode_robots']
     
-    while len(stack) > 0:
-        
-        starting_state = stack.pop()
-        
-        if starting_state.get_state()['time_left'] == 0:
-            cache[bp][starting_state.get_signature()] = state.get_state()['geodes_opened']
-            
-        
-        if starting_state.get_signature() not in visited:
-            
-            visited.add(starting_state.get_signature())
-            
-            if afford_robot(starting_state, 'ore', bp):
-                b_ore = buy_robot(starting_state, 'ore', bp)
-                b_ore = move_on(b_ore)
-                stack.append(b_ore)
-                
+    types = ['wait', 'ore', 'clay', 'obsidian', 'geode']
+    
+    new_states = [combined_move(state, type, bp) for type in types]
+    
+    maxval = max(maxval, max([dfs(new_state, bp) for new_state in new_states])) 
+    
+    
+    return maxval
+
+quality_level = 0
+for t in data:
+    quality_level += dfs(State(24,1,0,0,0,0,0,0,0), t) * t[0]
+print(quality_level)

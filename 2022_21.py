ff1dcb37fc9aaad6e0fb2a28466c8633c92ea458
hchulvej <hchulvej@gmail.com>
2023-01-16 15:07:32 +0100

    WIP

--- 2022_21.py
+++ 2022_21.py
@@ -77,3 +77,53 @@ if False:
 
 monkeys['humn']['value'] = None
 ready_monkeys.remove('humn')
+monkeys['root']['operation'] = '='
+waiting_monkeys.remove('root')
+
+# Get as many monkeys 'ready' as possible
+
+stop_checking = False
+
+while not stop_checking:
+    
+    wm = [m for m in waiting_monkeys]
+    
+    for monkey_name in wm:
+        monkey_business = monkeys[monkey_name]
+        if monkey_business['helper_monkeys'][0] in ready_monkeys and monkey_business['helper_monkeys'][1] in ready_monkeys:
+            val1 = monkeys[monkey_business['helper_monkeys'][0]]['value']
+            val2 = monkeys[monkey_business['helper_monkeys'][1]]['value']
+            if monkey_business['operation'] == '+':
+                monkey_business['value'] = val1 + val2
+                ready_monkeys.append(monkey_name)
+                waiting_monkeys.remove(monkey_name)
+            if monkey_business['operation'] == '-':
+                monkey_business['value'] = val1 - val2
+                ready_monkeys.append(monkey_name)
+                waiting_monkeys.remove(monkey_name)
+            if monkey_business['operation'] == '*':
+                monkey_business['value'] = val1 * val2
+                ready_monkeys.append(monkey_name)
+                waiting_monkeys.remove(monkey_name)
+            if monkey_business['operation'] == '/':
+                monkey_business['value'] = val1 // val2
+                ready_monkeys.append(monkey_name)
+                waiting_monkeys.remove(monkey_name)
+        else:
+            continue
+    
+    if len(waiting_monkeys) == len(wm):
+        stop_checking = True
+
+
+wm = [m for m in waiting_monkeys]
+nodes = []
+for monkey_name in wm:
+    res = []
+    res.append(monkey_name)
+    res.append(monkeys[monkey_name]['operation'])
+    for helper_monkey in monkeys[monkey_name]['helper_monkeys']:
+        res.append(tuple([helper_monkey, monkeys[helper_monkey]['value']]))
+    nodes.append(res)
+
+print([node for node in nodes if node[2][1] == None and node[3][1] == None])

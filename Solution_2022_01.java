ba5a8e3dffaf807956e3a0b73fb75d07f6c85a36
hchulvej <hchulvej@gmail.com>
2023-11-12 14:41:54 +0100

    Part one is done

--- Solution_2022_01.java
+++ Solution_2022_01.java
@@ -3,24 +3,32 @@ import java.util.Scanner;
 
 public class Solution_2022_01 {
 
-
    public static void main(String[] args) {
 
-        Scanner sc = null;
-        int maxCal = 0;
+      Scanner sc = null;
+      int maxCal = 0;
 
-        try {
+      try {
          sc = new Scanner(new File("2022_01.txt"), "utf-8");
-         
+         int elfLoad = 0;
          while (sc.hasNextLine()) {
-            System.out.println(sc.nextLine());
+            String line = sc.nextLine();
+
+            if (line.length() == 0) {
+               if (elfLoad > maxCal) {
+                  maxCal = elfLoad;
+               }
+               elfLoad = 0;
+            } else {
+               elfLoad += Integer.parseInt(line);
+            }
          }
 
-        } catch (Exception e) {
+      } catch (Exception e) {
          e.printStackTrace();
-        }
-        
-        System.out.println(maxCal);
+      }
+
+      System.out.println(maxCal);
    }
 
 }

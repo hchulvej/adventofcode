ee71b6bfcac92d804256c23c4e31ed83b2ba8189
hchulvej <hchulvej@gmail.com>
2023-12-02 20:23:06 +0100

    Day 2 underways

---
+++ 2023_02.py
@@ -0,0 +1,47 @@
+input = []
+
+with open('2023_02_1.txt', 'r') as f:
+    for l in f.readlines():
+        input.append(l.replace('\n', ''))
+
+
+games = []
+
+
+for i in range(len(input)):
+    games.append([x.strip() for x in input[i].split(':')[1].split(';')])
+
+
+def parse_game_example(s):
+    s = [x.strip().split(' ') for x in s.split(',')]
+    rgb = [0,0,0]
+    for i in range(len(s)):
+        if s[i][1] == "red":
+            rgb[0] = rgb[0] + int(s[i][0])
+        if s[i][1] == "green":
+            rgb[1] = rgb[1] + int(s[i][0])
+        if s[i][1] == "blue":
+            rgb[2] = rgb[2] + int(s[i][0])
+    return rgb
+
+def parse_game_input(game):
+    return [parse_game_example(x) for x in game]
+
+RED = 12
+GREEN = 13
+BLUE = 14
+
+def check_game(game):
+    res = True
+    for g in game:
+        if g[0] > RED or g[1] > GREEN or g[2] > BLUE:
+            res = False
+    return res
+
+score = 0
+
+for i in range(len(games)):
+    if check_game(parse_game_input(games[i])):
+        score = score + i + 1
+        
+print(score)

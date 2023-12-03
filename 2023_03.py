4a2ddde87fda4515551ff191a295233f0d98d2a0
hchulvej <hchulvej@gmail.com>
2023-12-03 13:25:14 +0100

    Day 3 WIP

--- 2023_03.py
+++ 2023_03.py
@@ -1,11 +1,11 @@
-import numpy as np
-
 input = []
 
 with open('2023_03_test.txt', 'r') as f:
     for l in f.readlines():
         input.append(l.replace('\n', ''))
 
-grid = np.array([list(x) for x in input])
 
-print(grid[0][0])
+def get_input(r, c):
+    return input[r][c]
+
+print(get_input(4,1))

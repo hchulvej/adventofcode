534f5165eb2b6a6c399960f8d6de28ee4003d771
hchulvej <hchulvej@gmail.com>
2023-12-16 14:11:41 +0100

    Day 3 done

--- 2023_03.py
+++ 2023_03.py
@@ -11,32 +11,39 @@ nc = len(input[0])
 print("Number of rows: " + str(nr))
 print("Number of columns: " + str(nc))
 
-def get_value(t):
-    return input[t[0]][t[1]]
+numbers = []
+symbols = []
 
-def adjacent_coordinates(r, c):
-    circle = [(-1, 0), (1, 0), (0, -1), (0, 1), (-1, -1), (-1, 1), (1, -1), (1, 1)]
-    return [(r + dr, c + dc) for dr, dc in circle if 0 <= r + dr < nr and 0 <= c + dc < nc]
-
-def valid_number(r, c):
-    return get_value((r, c)).isdigit() and len([get_value(t) for t in adjacent_coordinates(r,c) if not get_value(t).isdigit() and get_value(t) != '.']) > 0
-
-valid_numbers = []
-
-for r in range(nr):
-    number = ""
-    counts = False
-    for c in range(nc):
-        if get_value((r, c)).isdigit():
-            number += get_value((r, c))
-            if valid_number(r, c):
-                counts = True
-            if c == 139 and counts:
-                valid_numbers.append(int(number))
+for r, row in enumerate(input):
+    for c, col in enumerate(row):
+        if input[r][c] == '.':
+            continue
+        elif input[r][c].isdigit():
+            num = str(input[r][c])
+            inc = 1
+            while c + inc < nc and input[r][c + inc].isdigit():
+                num += str(input[r][c + inc])
+                inc += 1
+            if (c > 0 and not input[r][c-1].isdigit()) or c == 0:
+                numbers.append((int(num), r, c, c + inc - 1))
         else:
-            if (len(number) > 0) and counts:
-                valid_numbers.append(int(number))
-            counts = False
-            number = ""
+            symbols.append((input[r][c],(r,c)))
 
-print(sum(valid_numbers))
+part1 = 0
+part2 = 0
+
+for symbol, pos in symbols:
+    prod = 1
+    count = 0
+    for num in [n for n in numbers if n[1] in [pos[0] - 1, pos[0], pos[0] + 1] and pos[1] >= n[2] - 1 and pos[1] <= n[3] + 1]:
+        part1 += int(num[0])
+        if symbol == "*":
+            prod *= int(num[0])
+            count += 1
+    if count == 2:
+        part2 += prod
+            
+        
+
+print("Part 1: " + str(part1))
+print("Part 2: " + str(part2))

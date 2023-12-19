1e9ee261c5db82973e1ce5a5410ddefa982d9522
hchulvej <hchulvej@gmail.com>
2023-12-19 16:12:03 +0100

    Day 5 not done

--- 2023_05.py
+++ 2023_05.py
@@ -44,9 +44,19 @@ def intersection(intv, map_int):
     else:
         e, f  = map_int[1]
         return (e + le - c, e + re - c)
-    
 
-print(intersection((81, 95), ((25,95), (18,88))))
+seed_input_ranges = [set()]*8
+seed_input_ranges[0] = set([(seed_input[i], seed_input[i] + seed_input[i + 1]) for i in range(0, len(seed_input), 2)])
+
+
+for i in range(0, 7):
+    for map_int in map_inputs[i]:
+        for seed_int in seed_input_ranges[i]:
+            temp = set()
+            temp.add(intersection(seed_int, map_int))
+        seed_input_ranges[i + 1] = temp
+
+print(seed_input_ranges)
 
 
     

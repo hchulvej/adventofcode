c9b98471e80a73bf04ab16f253b4949542aab8cf
hchulvej <hchulvej@gmail.com>
2023-12-23 16:21:38 +0100

    New appraoch

--- 2023_07.py
+++ 2023_07.py
@@ -1,11 +1,18 @@
 import sys
 input = open("2023_07_test.in").read().splitlines()
-
 values = list("AKQJT98765432")
+
+class Card:
+    def __init__(self, card):
+        self.card = card
+        self.scorecard = dict(zip(values, [0]*len(values)))
+        for c in list(self.card):
+            self.scorecard[c] += 1
+        
+
 
 def score(card):
     first_card = card[0]
-    print(values.index(first_card))
     scorecard = dict(zip(values, [0]*len(values)))
     for c in list(card):
         scorecard[c] += 1
@@ -35,11 +42,29 @@ def score(card):
     # High card
     if scores == "11111":
         return (1, values.index(first_card))
-    
+
+def compare_cards(card1, card2):
+    score1 = score(card1)
+    score2 = score(card2)
+    if score1[0] > score2[0]:
+        return 1
+    elif score1[0] < score2[0]:
+        return -1
+    elif score1[0] == score2[0]:
+        if score1[1] > score2[1]:
+            return 1
+        elif score1[1] < score2[1]:
+            return -1
+        else:
+            return 0    
 
 cards = dict()
+card_ranks = dict()
 for line in input:
     pair = line.split(" ")
     cards[pair[0]] = int(pair[1])
 
+sorted_cards = sorted(cards.keys(), key=lambda x: score(x)[0], reverse=True)
+sorted_cards = sorted(sorted_cards, key=lambda x: score(x)[1], reverse=False)
 print(cards)
+print(sorted_cards)

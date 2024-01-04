5dfc2562c5a8a632c5eacb315a20b79b71001c0b
hchulvej <hchulvej@gmail.com>
2024-01-04 17:34:16 +0100

    Day 13 WIP

--- 2023_13.py
+++ 2023_13.py
@@ -1,15 +1,31 @@
-input = open("2023_13_test.in").read().splitlines()
-input.append("")
+grids = open("2023_13_test.in").read().split("\n\n")
 
-grids = []
+def rows(g):
+    return g.split("\n")
 
-grid = []
-for line in input:
-    if len(line) > 0:
-        grid.append(line)
-    else:
-        grids.append(grid)
-        grid = []
+def cols(g):
+    g_rows = rows(g)
+    return ["".join([g_rows[i][j] for j in range(len(g_rows[i]))]) for i in range(len(g_rows))]   
+    
+def array_comp(str_1, str_2, offset):
+    if len(str_1) != len(str_2):
+        return False
+    diffs = 0
+    
+    for i, e in enumerate(list(str_1)):
+        if e != str_2[i]:
+            diffs += 1
+    return offset == diffs
 
-for r in grids[1]:
-    print(r)
+def possible_lines(arr, offset):
+    return [i for i in range(len(arr) - 1) if array_comp(arr[i], arr[i + 1], offset)]
+
+def horizontal_lines(g):
+    return 0
+
+def vertical_lines(g):
+    return 0
+    
+for g in grids:
+    print(possible_lines(rows(g), 0))
+    print(possible_lines(cols(g), 0))    

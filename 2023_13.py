66eda27ee6cfd699bb9331fe2ec340cebb30da06
hchulvej <hchulvej@gmail.com>
2024-01-05 09:24:20 +0100

    Day 13 part 1

--- 2023_13.py
+++ 2023_13.py
@@ -1,4 +1,4 @@
-input = open("2023_13_test.in").read().splitlines()
+input = open("2023_13_1.in").read().splitlines()
 input.append("")
 
 grids = []
@@ -11,5 +11,30 @@ for line in input:
         grids.append(grid)
         grid = []
 
-for r in grids[1]:
-    print(r)
+def check_horizontal(g):
+    possible_lines = []
+    # Find line no
+    # line_i = line_(i+1)
+    for i in range(len(g) - 1):
+        if g[i] == g[i + 1]:
+            possible_lines.append(i)
+            
+    
+    # Check if all rows are reflected
+    for i in possible_lines:
+        if all([g[i - k] == g[i + 1 + k] for k in range(len(g)) if i - k >=0 and i + 1 + k < len(g)]):
+            return 100 * (i + 1)
+    
+    return 0
+    
+
+def check_vertical(g):
+    cols = [[g[i][j] for i in range(len(g))] for j in range(len(g[0]))]
+    return int(check_horizontal(cols) / 100)
+
+
+def score(g):
+    return check_horizontal(g) + check_vertical(g)
+    
+        
+print("Part 1:", sum([score(g) for g in grids]))

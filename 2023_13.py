2efc606e9db979d8fff87dd49c62d5b4b69f08d1
hchulvej <hchulvej@gmail.com>
2024-01-05 13:54:01 +0100

    Day 13 part 1 done

--- 2023_13.py
+++ 2023_13.py
@@ -1,31 +1,47 @@
-grids = open("2023_13_test.in").read().split("\n\n")
+input = open("2023_13_1.in").read().splitlines()
+input.append("")
 
-def rows(g):
-    return g.split("\n")
+grids = []
+temp_g = []
 
-def cols(g):
-    g_rows = rows(g)
-    return ["".join([g_rows[i][j] for j in range(len(g_rows[i]))]) for i in range(len(g_rows))]   
-    
-def array_comp(str_1, str_2, offset):
-    if len(str_1) != len(str_2):
+for line in input:
+    if len(line) == 0:
+        grids.append(tuple(temp_g))
+        temp_g = []
+    else:
+        temp_g.append(tuple(line))
+
+
+
+def no_rows(grid):
+    return len(grid)
+
+def no_cols(grid):
+    return len(grid[0])
+
+def cols(grid):
+    return tuple([tuple([grid[r][c] for r in range(no_rows(grid))]) for c in range(no_cols(grid))])
+
+def are_equal(arr_1, arr_2):
+    if len(arr_1) != len(arr_2):
         return False
-    diffs = 0
-    
-    for i, e in enumerate(list(str_1)):
-        if e != str_2[i]:
-            diffs += 1
-    return offset == diffs
+    for i, e in enumerate(arr_1):
+        if e != arr_2[i]:
+            return False
+    return True
 
-def possible_lines(arr, offset):
-    return [i for i in range(len(arr) - 1) if array_comp(arr[i], arr[i + 1], offset)]
+def is_reflection_line(grid, line_no):
+    return all([are_equal(grid[line_no - k], grid[line_no + 1 + k]) for k in range(no_rows(grid)) if line_no - k >= 0 and line_no + 1 + k < no_rows(grid)])
 
-def horizontal_lines(g):
-    return 0
+# Part 1
+score_p1 = 0
+for grid in grids:
+    for r in [r for r in range(no_rows(grid) - 1) if is_reflection_line(grid, r)]:
+        score_p1 += 100 * (r + 1)
+    cols_grid = cols(grid)
+    for c in [c for c in range(no_cols(grid) - 1) if is_reflection_line(cols_grid, c)]:
+        score_p1 += c + 1
+print("Part 1:", score_p1)
 
-def vertical_lines(g):
-    return 0
-    
-for g in grids:
-    print(possible_lines(rows(g), 1))
-    print(possible_lines(cols(g), 1))    
+# Part 2
+

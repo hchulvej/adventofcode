127c3b8ce236cf5be63f14c5c7481a729f8aeab0
hchulvej <hchulvej@gmail.com>
2024-01-05 16:29:36 +0100

    Day 13 complete

--- 2023_13_np.py
+++ 2023_13_np.py
@@ -13,31 +13,31 @@ for line in input:
         grids.append(np.array(temp_g))
         temp_g = []
     else:
-        temp_g.append(list(line))
+        temp_g.append(list(map(int, line.replace("#","0").replace(".", "1"))))
 
 transposed_grids = [grid.transpose() for grid in grids]
        
 
 # Helper functions
-def are_equal(np_arr_1, np_arr_2):
-    return all(np_arr_1 == np_arr_2)
+def no_differences(np_arr_1, np_arr_2):
+    return np.count_nonzero(np_arr_1 - np_arr_2)
 
-def is_reflection_line(grid, line_no):
-    return all([are_equal(grid[line_no - k], grid[line_no + 1 + k]) for k in range(len(grid)) if line_no - k >= 0 and line_no + 1 + k < len(grid)])
+def differences_reflection_line(grid, line_no):
+    return sum([no_differences(grid[line_no - k], grid[line_no + 1 + k]) for k in range(len(grid)) if line_no - k >= 0 and line_no + 1 + k < len(grid)])
 
-def horizontal_reflection_lines(grid_no):
-    grid = grids[grid_no]
-    return [r for r in range(len(grid) - 1) if is_reflection_line(grid, r)] 
-
-def vertical_reflection_lines(grid_no):
-    grid = transposed_grids[grid_no]
-    return [r for r in range(len(grid) - 1) if is_reflection_line(grid, r)]
 
 # Part 1
 score_p1 = 0
-for i in range(len((grids))):
-    score_p1 += sum([100*(r + 1) for r in horizontal_reflection_lines(i)])
-    score_p1 += sum([c + 1 for c in vertical_reflection_lines(i)])
-print("Part 1:", score_p1)
+for grid in grids:
+    score_p1 += sum([100 * (r + 1) for r in range(grid.shape[0] - 1) if differences_reflection_line(grid, r) == 0])
+for grid in transposed_grids:
+    score_p1 += sum([c + 1 for c in range(grid.shape[0] - 1) if differences_reflection_line(grid, c) == 0])
+print("Part 1:", score_p1)    
 
 # Part 2
+score_p2 = 0
+for grid in grids:
+    score_p2 += sum([100 * (r + 1) for r in range(grid.shape[0] - 1) if differences_reflection_line(grid, r) == 1])
+for grid in transposed_grids:
+    score_p2 += sum([c + 1 for c in range(grid.shape[0] - 1) if differences_reflection_line(grid, c) == 1])
+print("Part 2:", score_p2)

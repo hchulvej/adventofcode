b2173c0fbdb17e03d1afb8e750928964388f9e3c
hchulvej <hchulvej@gmail.com>
2024-12-06 16:56:38 +0100

    2024 Day 5 part 1

--- 2024_05.py
+++ 2024_05.py
@@ -9,7 +9,32 @@ raw_input = read_input("2024_05_2.txt")
 ordering = [tuple(map(int,s.split("|"))) for s in raw_input if s.count("|") == 1]
 updates = [tuple(map(int,s.split(","))) for s in raw_input if s.count(",") > 0]
 
-print(ordering)
-print(updates)
+# Part One
 
-print(raw_input)
+order = dict()
+for mi, ma in ordering:
+    if ma not in order:
+        order[ma] = set()
+    order[ma].add(mi)
+
+def is_ordered(arr, order):
+    ordered = True
+    for i in range(len(arr) - 1):
+        if arr[i] not in order:
+            continue
+        if arr[i + 1] in order[arr[i]]:
+            ordered = False
+            break
+    return ordered
+
+sum_part_one = 0
+for update in updates:
+    if is_ordered(update, order):
+        sum_part_one += update[len(update) // 2]
+
+print(sum_part_one)
+
+# Part Two
+
+not_in_order_updates = [update for update in updates if not is_ordered(update, order)]
+print(len(not_in_order_updates))

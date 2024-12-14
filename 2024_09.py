04e2806c796a946d5d573bd8745ff3020ca61a31
hchulvej <hchulvej@gmail.com>
2024-12-14 16:30:10 +0100

    2024 Day 9 part 2 wip

--- 2024_09.py
+++ 2024_09.py
@@ -3,100 +3,76 @@ def read_input(input_file):
         lines = [list(line.strip()) for line in f.readlines()]
     return lines
 
-raw_input = read_input("2024_09_1.txt")
+raw_input = read_input("2024_09_2.txt")
 
 data = list(map(int, raw_input[0]))
 
 # Part One
 
-def populate_file_system(data, part=1):
-    file = []
+def populate_file_system(data):
+    file_system = []
     
     current_index = 0
     for i, val in enumerate(data):
         if i % 2 == 0:
-            if part == 1:
-                file += [str(current_index)] * val
-            else:
-                file.append([str(current_index), val])
+            file_system += [str(current_index)] * val
             current_index += 1
         else:
-            if part == 1:
-                file += ["."] * val
-            else:
-                file.append(["Free space", val])
-        
-    return file
+            file_system += ["."] * val
+    return file_system
 
-def print_file_system(fs):
-    test = ""
 
-    for i, item in enumerate(fs):
-        if item[0] == "Free space":
-            for _ in range(item[1]):
-                test += "."
-        else:
-            for _ in range(item[1]):
-                test += item[0]    
-    return test
-
-def combine_free_space(fs):
-    for _ in range(len(fs)):
-        for i, item in enumerate(fs):
-            if item[0] == "Free space" and i < len(fs) - 1 and fs[i + 1][0] == "Free space":
-                fs[i][1] += fs[i + 1][1]
-                fs.pop(i + 1)
-    return fs
-                  
-
-def compact_file_system(file, part=1):
+def compact_file_system(file_system, part=1):
     if part == 1:
         left_index = 0
-        right_index = len(file) - 1
+        right_index = len(file_system) - 1
         while left_index < right_index:
-            while file[left_index] != ".":
+            while file_system[left_index] != ".":
                 left_index += 1
-            while file[right_index] == ".":
+            while file_system[right_index] == ".":
                 right_index -= 1
             if left_index < right_index:
-                tmp = file[right_index]
-                file[right_index] = "."
-                file[left_index] = tmp
-        return file
+                tmp = file_system[right_index]
+                file_system[right_index] = "."
+                file_system[left_index] = tmp
+        return file_system
     else:
-        right_index = len(file) - 1
-        while right_index >= 0:
-            while file[right_index][0] == "Free space":
-                right_index -= 1
-            for i in range(right_index):
-                if file[i][0] != "Free space" or file[i][1] < file[right_index][1]:
-                    continue
-                else:
-                    tmp = file[right_index].copy()
-                    file[right_index][0] = "Free space"
-                    file.insert(i, tmp)
-                    file[i + 1][1] -= tmp[1]
-                    file = combine_free_space(file)
-            right_index -= 1
-        return file
+        return file_system
                     
 
-def score(data_file):
-    compacted_file = compact_file_system(populate_file_system(data_file))
+def score(file_system):
+    compacted_file = compact_file_system(file_system)
     eof = compacted_file.index(".")
     checksum = 0
     for i in range(eof):
         checksum += int(compacted_file[i]) * i
     return checksum
         
+file_system = populate_file_system(data)
+
+print(file_system)
     
-print(score(data))    
+#print(score(data))    
 
 
 # Part Two
 
-free_space = [(i, item) for i, item in enumerate(populate_file_system(data, part=2)) if item[0] == "Free space"]
-print(len(free_space))
+def free_space_groups(fs):
+    groups = []
+    for index, item in enumerate(fs):
+        if item == "." and (index > 1 and fs[index - 1] != "."):
+            min_index = index
+            length = 1
+            while (index + length < len(fs) and fs[index + length] == "."):
+                length += 1
+            groups.append([min_index, length])
+    return groups
+
+print(free_space_groups(file_system))
+
+def combine_free_space(fs):
+    return
+
       
 
           

4bc85c0c2b360a45b20cbb1b8d13c574f478c72c
hchulvej <hchulvej@gmail.com>
2024-12-15 11:43:54 +0100

    2024 Day 9 part 2 still wrong

--- 2024_09.py
+++ 2024_09.py
@@ -3,7 +3,7 @@ def read_input(input_file):
         lines = [list(line.strip()) for line in f.readlines()]
     return lines
 
-raw_input = read_input("2024_09_2.txt")
+raw_input = read_input("2024_09_1.txt")
 
 data = list(map(int, raw_input[0]))
 
@@ -48,12 +48,51 @@ def score(file_system):
         checksum += int(compacted_file[i]) * i
     return checksum
         
-print(score(populate_file_system(data)))    
+print(f"Score part one: {score(populate_file_system(data))}")    
 
 
 # Part Two
 
+file_system = populate_file_system(data)
 
+def min_index_free_space(fs, fl):
+    left_index = 0
+    try:
+        left_index = fs.index(".", left_index)
+    except ValueError:
+        left_index = len(fs)
+    while left_index + fl < len(fs):
+        if fs[left_index:left_index + fl].count(".") >= fl:
+            return left_index
+        try:
+            left_index = fs.index(".", left_index + fl)
+        except ValueError:
+            left_index = len(fs)
+    return len(fs)
+    
       
+i = len(file_system) - 1
+moved = set()
+while i >=0:
+    if file_system[i] == ".":
+        i -= 1
+    else:
+        f_name = file_system[i]
+        f_length = file_system.count(f_name)
+        fs_min_index = min_index_free_space(file_system, f_length)
+        if fs_min_index < len(file_system) and (f_name not in moved) and fs_min_index < i:
+            moved.add(f_name)
+            for j in range(f_length):
+                file_system[fs_min_index + j] = f_name
+                file_system[i - j] = "."
+        else:
+            i -= f_length
+            
+score_part_two = 0
 
+for i, item in enumerate(file_system):
+    if item != ".":
+        score_part_two += int(item) * i
+
+print(f"Score part two: {score_part_two}")
           

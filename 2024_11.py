bcf5677d44d90ee0186a3fe3c93839f534f8ae31
hchulvej <hchulvej@gmail.com>
2024-12-17 09:31:44 +0100

    2024 Day 11 trouble

--- 2024_11.py
+++ 2024_11.py
@@ -5,20 +5,31 @@ def read_input(input_file):
         lines = [list(map(int, line.split(" "))) for line in f.readlines()]
     return lines
 
-raw_input = np.array(read_input("2024_11_2.txt")[0])
+raw_input = np.array(read_input("2024_11_1.txt")[0])
 
 arrangement = raw_input
 
-def split_stone(arr, index):
-    val = str(arr[index])
-    left, right = int(val[:len(val)//2 + 1]), int(val[(len(val) // 2 + 1):])
-    return [left, right]
-
 def blink(arr):
-    # Todo: Make new result array and return it
-    pass
+    res = []
+    for val in arr:
+        if val == 0:
+            res.append(1)
+        elif len(str(val)) % 2 == 0:
+            s_val = str(val)
+            s_left = s_val[:len(s_val)//2]
+            s_right = s_val[(len(s_val) // 2):]
+            left = int(s_left)
+            right = int(s_right)
+            if left < 0 or right < 0:
+                print(s_val)
+            res += [left, right]
+        else:
+            res.append(val * 2024)
+    return res
                 
 
 print(arrangement)
-arrangement = blink(arrangement)
-print(arrangement)
+for _ in range(25):
+    arrangement = blink(arrangement)
+    #print(len(arrangement))
+print(len(arrangement))

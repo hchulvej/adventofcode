da3660c11469ad76b25676c10593038932a31bfc
hchulvej <hchulvej@gmail.com>
2024-12-21 16:14:42 +0100

    2024 Day 13 done

--- 2024_13.py
+++ 2024_13.py
@@ -1,20 +1,21 @@
 
-import re
+import re as regex
+import sympy
 
 def read_input(input_file):
     with open(input_file) as f:
         lines = [line.strip() for line in f.readlines()]
     return lines
 
-raw_input = read_input("2024_13_2.txt")
+raw_input = read_input("2024_13_1.txt")
 
 PRICE_A, PRICE_B = 3, 1
 
 patterns = dict()
-patterns["X+"] = re.compile(r'X\+\d+')
-patterns["Y+"] = re.compile(r'Y\+\d+')
-patterns["X="] = re.compile(r'X\=\d+')
-patterns["Y="] = re.compile(r'Y\=\d+')
+patterns["X+"] = regex.compile(r'X\+\d+')
+patterns["Y+"] = regex.compile(r'Y\+\d+')
+patterns["X="] = regex.compile(r'X\=\d+')
+patterns["Y="] = regex.compile(r'Y\=\d+')
 
 claw_machines = []
 
@@ -36,6 +37,37 @@ for i in range(len(raw_input)):
     if len(input) == 0:
         continue
         
-    
 
-print(claw_machines)
+def solve(claw_machine):
+    coeff_matrix = sympy.Matrix([[claw_machine["A"][0], claw_machine["B"][0]], [claw_machine["A"][1], claw_machine["B"][1]]])
+    const_matrix = sympy.Matrix([claw_machine["P"][0], claw_machine["P"][1]])
+    if coeff_matrix.det() == 0:
+        return "To be checked"
+    else:
+        a, b = coeff_matrix**(-1) * const_matrix
+        return (a, b, a.is_integer and b.is_integer)   
+
+
+# Part One
+
+tokens = 0
+for cm in claw_machines:
+    s = solve(cm)
+    if s[2]:
+        tokens += s[0] * PRICE_A + s[1] * PRICE_B
+print(tokens)
+
+
+# Part Two
+
+OFFSET = 10000000000000
+
+for cm in claw_machines:
+    cm["P"] = (cm["P"][0] + OFFSET, cm["P"][1] + OFFSET)
+
+tokens = 0
+for cm in claw_machines:
+    s = solve(cm)
+    if s[2]:
+        tokens += s[0] * PRICE_A + s[1] * PRICE_B
+print(tokens)

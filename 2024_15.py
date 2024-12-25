f1f2714277678870930081b4126eae8bd93e1d4e
hchulvej <hchulvej@gmail.com>
2024-12-25 17:02:45 +0100

    Day 15 part 2 wip

--- 2024_15.py
+++ 2024_15.py
@@ -104,50 +104,100 @@ print(sum(b[0] + 100 * b[1] for b in boxes))
 
 # Part Two
 
-walls = set()
-boxes = set()
-robot = [(0,0)]
+first_line = raw_input[0]
+DIM_Y = 1
+for line in raw_input[1:]:
+    if line == first_line:
+        break
+    DIM_Y += 1
 
-for y, line in enumerate(raw_input):
-    if line.count("#") > 0:
-        for x, char in enumerate(line):
-            if char == "#":
-                walls |= {(2 * x, y), (2 * x + 1, y)}
-            elif char == "O":
-                boxes.add((2 * x, y))
-            elif char == "@":
-                robot[0] = (2 * x, y)
+DIM_X = 2 * len(first_line)
+the_map = [["." for _ in range(DIM_X)] for _ in range(DIM_Y)]
 
+for y, line in enumerate(raw_input[:DIM_Y]):
+    for x, char in enumerate(line):
+        if char == "#":
+            the_map[y][2 * x] = "#"
+            the_map[y][2 * x + 1] = "#"
+        elif char == "O":
+            the_map[y][2 * x] = "["
+            the_map[y][2 * x + 1] = "]"
+        elif char == "@":
+            the_map[y][2 * x] = "@"
+            the_map[y][2 * x + 1] = "."
 
+def can_push_box(left_box, direction):
+    lx, ly = left_box
+    rx, ry = left_box[0] + 1, left_box[1]
+    
+    return True
 
-def affected_boxes(set_of_boxes, first_box, direction):
-    aff_boxes = set(first_box)
+def push_box(left_box, direction):
     dx, dy = DIRS[direction]
-    f_x, f_y = first_box #Left side coordinates "["
-    # Case 1: Horsiontal movement
-    if direction in ["<", ">"]:
-        m = 0
-        while (f_x + 2 * m * dx, f_y + m * dy) in set_of_boxes:
-            aff_boxes.add((f_x + 2 * m * dx, f_y + m * dy))
-            m += 1
-    # Case 2: Vertical movement
-    else:
-        queue = deque(first_box)
-        while len(queue) > 0:
-            x, y = queue.pop()
-            # []
-            # []
-            if (x, y + dy) in set_of_boxes:
-                aff_boxes.append((x, y + dy))
-            # [][]
-            #  []
-            if (x - 1, y + dy) in set_of_boxes:
-                aff_boxes.append((x - 1, y + dy))
-            if (x + 1, y + dy) in set_of_boxes:
-                aff_boxes.append((x + 1, y + dy))
-    return aff_boxes
+    lx, ly = left_box
+    rx, ry = left_box[0] + dx, left_box[1] + dy
+    if can_push_box(left_box, direction):
+        the_map[ly][lx] = "."
+        the_map[ry][rx] = "."
+        the_map[ly + dy][lx + dx] = "["
+        the_map[ry + dy][rx + dx] = "]"
+    return True
+    
 
-print(boxes)
-print(list(affected_boxes(boxes, (6,3), "v")))
+def push_boxes(left_box, direction):
+    dx, dy = DIRS[direction]
+    lx, ly = left_box
+    if dy == 0:
+        # Horizontal push
+        queue = [[lx, ly]]
+        c = 1
+        while the_map[ly][lx + c * 2 * dx] == "[": 
+            queue.append([(lx + c * 2 * dx, ly)])
+            c += 1
+        last_box_left = queue[-1]
+        if can_push_box(last_box_left, direction):
+            while len(queue) > 0:
+                box = queue.pop()
+                push_box(box, direction)
+    else:
+        # Vertical push
+        affected_boxes = [[lx, ly]]
+        queue = [[lx, ly]]
+        c = 1
+        while len(queue) > 0:
+            lx, ly = queue.pop()
+            if the_map[ly + c * dy][lx] == "[":
+                affected_boxes.append([lx, ly + c * dy])
+                queue.append([(lx, ly + c * dy)])
+                c += 1
+            if the_map[ly + c * dy][lx] == "]":
+                affected_boxes.append([lx - 1, ly + c * dy])
+                affected_boxes.append([lx + 1, ly + c * dy])
+                queue.append([(lx - 1, ly + c * dy)])
+                queue.append([(lx + 1, ly + c * dy)])
+                c += 1
+        if all(can_push_box(box, direction) for box in affected_boxes):
+            if direction == "^":
+                affected_boxes.sort(key=lambda x: x[1], reverse=True)
+            else:
+                affected_boxes.sort(key=lambda x: x[1])
+            while len(affected_boxes) > 0:
+                box = affected_boxes.pop()
+                push_box(box, direction)
+            
+        
+            
+    
+
+def draw_map_part_2(a_map):
+    for y in range(DIM_Y):
+        print("".join(a_map[y]))
+
+
+
+
+
+print(draw_map_part_2(the_map))
+print(can_push_box((16,1), "<"))
         
         

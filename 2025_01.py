78a92fd9734e5928b0a8916f51c5fe2838dd32b1
hchulvej <hchulvej@gmail.com>
2025-12-02 09:26:25 +0100

    OK

--- 2025_01.py
+++ 2025_01.py
@@ -1,5 +1,47 @@
-import os
+# We load the data file
+with open("2025_01.txt") as f:
+    lines = f.read().splitlines()
 
-BASE = os.path.dirname(__file__)
+def change_rotation(rotation):
+    if rotation[0] == "L":
+        temp = int(rotation[1:])
+        temp = 100 - temp
+        return "R" + str(temp)
+    return rotation
 
-print(os.path.join(BASE, "2025_01.txt"))
+def move_dial(dial, rotation):
+    rotation = change_rotation(rotation)
+    rot = int(rotation[1:])
+    return (dial + rot) % 100
+
+## Part 1
+starting_dial = 50
+number_of_zeros = 0
+for line in lines:
+    starting_dial = move_dial(starting_dial, line)
+    if starting_dial == 0:
+        number_of_zeros += 1
+
+print("Password:", number_of_zeros)
+
+## Part 2
+def zero_in_positions(dial, rotation):
+    rot = int(rotation[1:])
+    if rotation[0] == "R":
+        return (dial + rot) // 100
+    else:
+        times = 0
+        while dial - rot < 0:
+            dial += 100
+            times += 1
+            return True
+    return 0
+        
+
+starting_dial = 50
+zeroes_hit = 0
+for line in lines:
+    zeroes_hit += zero_in_positions(starting_dial, line)
+    starting_dial = move_dial(starting_dial, line)
+print("Zeroes hit:", zeroes_hit)
+    

00909034fb788729e7efc788c41ebaa264dfff7d
hchulvej <hchulvej@gmail.com>
2025-12-14 17:48:20 +0100

    Done

--- 2025_06.py
+++ 2025_06.py
@@ -1,4 +1,4 @@
-def read_data():
+def read_data_p1():
     with open("2025_06.txt") as f:
         return_data = []
         for item in f.read().split("\n"):
@@ -13,7 +13,7 @@ def read_data():
             return_data.append(items)
         return return_data
 
-data = read_data()
+data = read_data_p1()
 
 ## Part 1
 def grand_total(data):
@@ -29,3 +29,53 @@ def grand_total(data):
     return total
 
 print("Part 1: The grand total is", grand_total(data))
+
+## Part 2
+def read_data_p2():
+    with open("2025_06.txt") as f:
+        return f.read().split("\n")
+
+data2 = read_data_p2()
+
+sign_placements = []
+
+for i in range(len(data2[4])):
+    if data2[4][i] in ["+", "*"]:
+        sign_placements.append(i)
+        
+number_placements = []
+
+for j in range(len(sign_placements) - 1):
+    number_placements.append((data2[4][sign_placements[j]], sign_placements[j], sign_placements[j + 1] - 2))
+
+number_placements.append((data2[4][sign_placements[-1]], sign_placements[-1], len(data2[0]) - 1))
+    
+def get_values(no_placement):
+    values = []
+    for c in range(no_placement[1], no_placement[2] + 1):
+        col = []
+        for r in range(4):
+            if data2[r][c] == " ":
+                col.append("")
+            else:
+                col.append(data2[r][c])
+        values.append(int("".join(col)))
+        
+    return values
+
+
+## Part 2
+def grand_total2():
+    total = 0
+    for no_placement in number_placements:
+        values = get_values(no_placement)
+        if no_placement[0] == "+":
+            total += sum(values)
+        if no_placement[0] == "*":
+            prod = 1
+            for val in values:
+                prod *= val
+            total += prod
+    return total
+
+print("Part 2: The grand total is", grand_total2())

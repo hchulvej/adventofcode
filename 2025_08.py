b9364322aea2209315ac0f774f9d4fed6e3cb856
hchulvej <hchulvej@gmail.com>
2025-12-26 16:07:59 +0100

    OMW

--- 2025_08.py
+++ 2025_08.py
@@ -1,4 +1,4 @@
-import numpy as np
+from collections import Counter
 
 def read_data():
     with open("2025_08_test.txt") as f:
@@ -12,42 +12,23 @@ def distance_calc(p1, p2):
 
 data = read_data()
 
-# Udregner og gemmer alle afstande i en opslagstabel
-calculated_distances = { i:[distance_calc(data[i], data[j]) for j in range(i + 1,len(data))] for i in range(len(data) - 1) }
+# Udregner og gemmer alle afstande mellem to punkter i en opslagstabel
+calculated_distances = { (i,j) : distance_calc(data[i], data[j]) for i in range(len(data)) for j in range(i + 1, len(data)) }
 
 # Funktion til at slå afstand op mellem to punkter ved hjælp af opslagstabellen
 def distance_lookup(i, j):
     if i < j:
-        return calculated_distances[i][j - i - 1]
+        return calculated_distances[(i,j)]
     elif i > j:
         return distance_lookup(j, i)
     else:
         return 0
 
-# Matricen indeholdende alle afstande mellem punkter
-distance_matrix = np.array([ [ distance_lookup(i,j) for j in range(len(data)) ] for i in range(len(data)) ])
+# Vi sorterer punktparrene baseret på deres afstande, mindste afstand først
+sorted_distances = sorted(calculated_distances.keys(), key=lambda pair: calculated_distances[pair])
 
-# Punkternes gruppering baseret på afstande
-gruppering = { i : [data[i]] for i in range(len(data)) }
+for i, j in sorted_distances:
+    print("i = " + str(i), "j = " + str(j), "Distance squared:", calculated_distances[(i,j)])
 
 
-
-def grupper_punkterne():
-    mindsteafstand = 0
-    runde = 0
-    while runde < len(data):
-        min_afstand = distance_matrix[distance_matrix > mindsteafstand].min()
-        indeks = np.where(distance_matrix == min_afstand)
-        for inx in indeks:
-            p1, p2 = inx[0], inx[1]
-            if p1 < p2:
-                gruppering[p1].extend(gruppering[p2])
-                gruppering[p2] = []
-                print(f"Runde {runde}: Punkt {p2} er nu en del af gruppe {p1}")
-        mindsteafstand = min_afstand
-        runde += 1
-
-grupper_punkterne()
-print(gruppering)
-print({ i : len(gruppering[i]) for i in range(len(data)) if len(gruppering[i]) > 0 })
 

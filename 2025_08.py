6bbc3ebf83534ba7824975501da96e3989d8f668
hchulvej <hchulvej@gmail.com>
2025-12-27 10:27:45 +0100

    Done

--- 2025_08.py
+++ 2025_08.py
@@ -1,7 +1,8 @@
 from collections import Counter
+import math
 
 def read_data():
-    with open("2025_08_test.txt") as f:
+    with open("2025_08.txt") as f:
         return [[int(n) for n in l.split(",")] for l in f.read().split("\n")]
 
 ## Part 1 ##
@@ -27,8 +28,57 @@ def distance_lookup(i, j):
 # Vi sorterer punktparrene baseret på deres afstande, mindste afstand først
 sorted_distances = sorted(calculated_distances.keys(), key=lambda pair: calculated_distances[pair])
 
-for i, j in sorted_distances:
-    print("i = " + str(i), "j = " + str(j), "Distance squared:", calculated_distances[(i,j)])
+# Vi har lånt en datastruktur fra union-find algoritmen til at holde styr på hvilke punkter der er forbundet
+class UnionFind:
+    def __init__(self, size):
+      
+        # Initialize the parent array with each 
+        # element as its own representative
+        self.parent = list(range(size))
+    
+    def find(self, i):
+      
+        # If i itself is root or representative
+        if self.parent[i] == i:
+            return i
+          
+        # Else recursively find the representative 
+        # of the parent
+        return self.find(self.parent[i])
+    
+    def unite(self, i, j):
+      
+        # Representative of set containing i
+        irep = self.find(i)
+        
+        # Representative of set containing j
+        jrep = self.find(j)
+        
+        # Make the representative of i's set
+        # be the representative of j's set
+        self.parent[irep] = jrep
 
+# Vi forbinder de 10 tætteste punkter
+uf = UnionFind(len(data))
+for (i, j) in sorted_distances[:1000]:
+    uf.unite(i, j)
 
+values = [uf.find(i) for i in range(len(data))]
+counter = Counter(values)
 
+print("Part 1:", math.prod([e[1] for e in counter.most_common(3)]))
+
+## Part 2 ##
+
+# Vi fortsætter med at forbinde punkter, indtil alle punkter er forbundet
+uf2 = UnionFind(len(data))
+for (i, j) in sorted_distances:
+    uf2.unite(i, j)
+    # Finder alle værdier
+    values = [uf2.find(k) for k in range(len(data))]
+    # Tæller forekomsterne af hver værdi
+    counter = Counter(values)
+    # Hvis der kun er én unik værdi, er alle punkter forbundet
+    if len(counter) == 1:
+        print("Part 2:", data[i][0] * data[j][0])
+        break

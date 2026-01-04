a10437641a868199b43eef94f9012f984aeab067
hchulvej <hchulvej@gmail.com>
2026-01-04 13:35:18 +0100

    Done

--- 2025_12.py
+++ 2025_12.py
@@ -3,7 +3,7 @@ def read_data():
     shapes = []
     regions = []
     line_no = 0
-    with open("2025_12_test.txt") as f:
+    with open("2025_12.txt") as f:
         
         for line in f.read().split("\n"):
             if line_no < 30 and line_no % 5 != 0 and line_no % 5 != 4:
@@ -20,4 +20,17 @@ def read_data():
 
 data = read_data()
 
-print(data[0])
+## Part 1 ##
+
+def fit_boxes(region):
+    shape = list(region.keys())[0]
+    space_no_of_boxes = (shape[0] // 3) * (shape[1] // 3)
+    needed_no_of_boxes = sum(list(region.values())[0])
+    return space_no_of_boxes >= needed_no_of_boxes
+
+no_of_regions = 0
+for region in data[1]:
+    if fit_boxes(region):
+        no_of_regions += 1
+
+print("Part 1:", no_of_regions)
